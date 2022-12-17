98b159540f9a786caef4a02b75e880bcc53c8c4c
MylesXP <mylesparkinson12@gmail.com>
2022-12-17 01:08:04 -0800

    Created Divs in JS instead of HTML, Added Drawing
    Delete hard-coded, temporary divs in HTML used for developmental purposes. Re-created said divs in JS using functions.

--- app.js
+++ app.js
@@ -1,7 +1,43 @@
-let pixel = document.querySelectorAll('.div-pixel');
+// Declaring Variables
+let pixelContainer = document.getElementById('div_container');
 
-pixel.forEach( px => {
-    px.addEventListener('click', () => {
-        px.classList.add('selected');
-    });
-});
+
+// Creating Div 'Pixels' for 16x16 Grid
+function createPixels () {
+    let pixelLine = document.createElement('div');
+    for (let i = 0; i < 16; i++) {
+        let pixel = document.createElement('div');
+        pixelLine.appendChild(pixel);
+        pixel.classList.add('div-pixel');
+        pixel.textContent = '1';
+    };
+    pixelLine.classList.add('div-line');
+    pixelContainer.appendChild(pixelLine);
+
+
+    // Adding 'Drawing' Logic
+    let pixelLineChildren = pixelLine.children
+    for (const child of pixelLineChildren){
+        child.addEventListener('mouseover', () => {
+            child.classList.add('selected');
+        });
+    }
+};
+
+
+// Creating 16 Different Lines of Pixels, Containing 16 Pixels Each
+for (let i = 0; i < 16; i++) {
+    createPixels();
+};
+
+
+
+
+
+
+
+// pixel.forEach( px => {
+//     px.addEventListener('mouseover', () => {
+//         px.classList.add('selected');
+//     });
+// });
