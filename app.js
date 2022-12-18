c1c273ee47793a349c508e5885fec327caed56cd
MylesXP <mylesparkinson12@gmail.com>
2022-12-18 10:53:42 -0800

    Added Button Logic
    Added logic for the user input custom size button. Rewrote the createPixels function slightly to accept an argument Input which is given by the prompt from the func declaration userInput

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
+
 // Declaring Variables
 let pixelContainer = document.getElementById('div_container');
-
+// let userInput = prompt('Enter a Number Up to 32');
+let userInput = function() {
+    pixelContainer.innerHTML = '';
+    let input = prompt('Enter a Number Up to 32');
+    for (let i = 0; i < input; i++) {
+        createPixels(input);
+    };
+};
 
 // Creating Div 'Pixels' for 16x16 Grid
-function createPixels () {
+function createPixels (Input) {
     let pixelLine = document.createElement('div');
-    for (let i = 0; i < 40; i++) {
+    for (let i = 0; i < Input; i++) {
         let pixel = document.createElement('div');
         pixelLine.appendChild(pixel);
         pixel.classList.add('div-pixel');
-        // pixel.textContent = ".";
+        
     };
     pixelLine.classList.add('div-line');
+    pixelLine.style = `grid-template-columns: repeat(${Input}, auto)`
     pixelContainer.appendChild(pixelLine);
-
+    pixelContainer.style = `grid-template-rows: repeat(${Input}, auto)`
 
     // Adding 'Drawing' Logic
     let pixelLineChildren = pixelLine.children
@@ -25,19 +34,47 @@ function createPixels () {
 };
 
 
-// Creating 16 Different Lines of Pixels, Containing 16 Pixels Each
-for (let i = 0; i < 40; i++) {
-    createPixels();
-};
 
 
 
 
 
 
+// // Declaring Variables
+// let pixelContainer = document.getElementById('div_container');
+// // let userInput = prompt('Enter a Number Up to 32');
+// let userInput;
 
-// pixel.forEach( px => {
-//     px.addEventListener('mouseover', () => {
-//         px.classList.add('selected');
-//     });
-// });
+// function getUserInput() {
+//     userInput = prompt('Enter a Number Up to 32');
+// }
+
+// getUserInput();
+
+// // Creating 16 Different Lines of Pixels, Containing 16 Pixels Each
+// for (let i = 0; i < userInput; i++) {
+//     createPixels(userInput);
+// };
+
+// // Creating Div 'Pixels' for 16x16 Grid
+// function createPixels (Input) {
+//     let pixelLine = document.createElement('div');
+//     for (let i = 0; i < Input; i++) {
+//         let pixel = document.createElement('div');
+//         pixelLine.appendChild(pixel);
+//         pixel.classList.add('div-pixel');
+        
+//     };
+//     pixelLine.classList.add('div-line');
+//     pixelLine.style = `grid-template-columns: repeat(${Input}, auto)`
+//     pixelContainer.appendChild(pixelLine);
+//     pixelContainer.style = `grid-template-rows: repeat(${Input}, auto)`
+
+//     // Adding 'Drawing' Logic
+//     let pixelLineChildren = pixelLine.children
+//     for (const child of pixelLineChildren){
+//         child.addEventListener('mouseover', () => {
+//             child.classList.add('selected');
+//         });
+//     }
+// };
