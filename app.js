cccd4c09eeb4d40b9475ad9f6a9aa5d0a0cfa068
MylesXP <mylesparkinson12@gmail.com>
2022-12-18 22:02:46 -0800

    Added reset button, grid stays same size on all grid sizes
    Added reset button & its logic, so when the button is clicked, it runs resetButton function. Also made the grid div stay the same size, irregardless of the user given grid size. Set up if statements to catch letters, empty strings, 0 numbers larger than 100, which is the limit.

--- app.js
+++ app.js
@@ -1,31 +1,68 @@
 
 // Declaring Variables
 let pixelContainer = document.getElementById('div_container');
-// let userInput = prompt('Enter a Number Up to 32');
+
+// Defining & Initializing Default Grid Size
+function defaultGrid () {
+    for (let i = 0; i < 16; i++) {
+        createPixels(16);
+    };
+}
+
+defaultGrid();
+
+// Logic for Reset Button
+function resetButton () {
+    let pixelContainerPixels = document.querySelectorAll(".div-pixel")
+    for (const child of pixelContainerPixels){
+        child.classList.remove('selected')
+    }
+}
+
+// Capturing User Input & Passing it to the createPixels Function to Create Grid
 let userInput = function() {
     pixelContainer.innerHTML = '';
-    let input = prompt('Enter a Number Up to 32');
-    for (let i = 0; i < input; i++) {
-        createPixels(input);
-    };
+    let input = Number(prompt('Enter a Number Up to 100'));
+    if (input > 100) {
+        alert('Enter a valid number');
+        defaultGrid()
+    } else if (!input) {
+        alert('Enter a number');
+        defaultGrid()
+    } else if (input === 0) {
+        alert('0 is invalid. Please Enter a Valid Number');
+        defaultGrid()
+    }  else if (typeof input === 'string') {
+        alert('Enter a number');
+        defaultGrid()
+    }
+    else {
+        for (let i = 0; i < input; i++) {
+            createPixels(input);
+        };
+    }
 };
 
-// Creating Div 'Pixels' for 16x16 Grid
+
+// Function for Creating Div 'Pixels' & its Rows for 16x16 Grid
 function createPixels (Input) {
     let pixelLine = document.createElement('div');
+
+    // Pixel Creation
     for (let i = 0; i < Input; i++) {
         let pixel = document.createElement('div');
         pixelLine.appendChild(pixel);
         pixel.classList.add('div-pixel');
-        
     };
+
+    // Grid Creation Logic
     pixelLine.classList.add('div-line');
     pixelLine.style = `grid-template-columns: repeat(${Input}, auto)`
     pixelContainer.appendChild(pixelLine);
     pixelContainer.style = `grid-template-rows: repeat(${Input}, auto)`
 
-    // Adding 'Drawing' Logic
-    let pixelLineChildren = pixelLine.children
+    // 'Drawing' Logic
+    let pixelLineChildren = pixelLine.childNodes
     for (const child of pixelLineChildren){
         child.addEventListener('mouseover', () => {
             child.classList.add('selected');
